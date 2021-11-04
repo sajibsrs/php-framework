ac4b122b85dd77e589d903a46d85e47e2612ebae
Sajidur Rahman <sajibsrs@gmail.com>
2021-11-05 02:49:16 +0600

    added connection class

---
+++ Application/Database/Connection.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Database;
+
+use PDO;
+use Exception;
+use PDOException;
+
+class Connection
+{
+    const ERROR_CONFIG = 'ERROR: Database configuration error';
+
+    public $pdo;
+
+    public function __construct(array $config)
+    {
+        if (! isset($config)) {
+            $error = __METHOD__ . ' : ' . self::ERROR_CONFIG . PHP_EOL;
+            throw new Exception($error);
+        }
+
+        $dsn = $config['driver'] . ':' . 'host=' . $config['host'] . ';' . 'dbname=' . $config['dbname'];
+
+        try {
+            $this->pdo = new PDO(
+                $dsn,
+                $config['user'],
+                $config['password'],
+                [PDO::ATTR_ERRMODE => $config['errmode']]
+            );
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
+            return false;
+        }
+    }
+}

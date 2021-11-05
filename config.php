bbf3efbb2b95bdbc795d1f4a1c46727954b1082e
Sajidur Rahman <sajibsrs@gmail.com>
2021-11-06 04:09:43 +0600

    configuration file added

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbconfig = [
+    'driver'    => 'pgsql',
+    'host'      => 'localhost',
+    'user'      => 'postgres',
+    'password'  => 'password',
+    'dbname'    => 'laravel_postgres_redis_user_data'
+];

e4d8421d620974c1695af3a3f34d92d96ecd03ca
Sajidur Rahman <sajibsrs@gmail.com>
2021-11-09 04:36:13 +0600

    added: database operation methods

--- Application/Entity/CustomerService.php
+++ Application/Entity/CustomerService.php
@@ -5,6 +5,8 @@ namespace Application\Entity;
 use Application\Database\Connection;
 use Application\Database\QueryBuilder;
 use PDO;
+use PDOException;
+use Throwable;
 
 class CustomerService
 {
@@ -54,28 +56,111 @@ class CustomerService
         );
     }
 
-    public function save(Customer $customer)
+    /**
+     * For new customer adds a new customer in the database.
+     * If existing customer updated the data.
+     * 
+     * @param \Application\Entity\Customer $customer Customer instance
+     * @return bool True on success and false on failure
+     */
+    public function save(Customer $customer): bool
     {
-
+        if ($customer->getId() && $this->findById($customer->getId())) {
+            return $this->update($customer);
+        } else {
+            return $this->insert($customer);
+        }
     }
 
+    /**
+     * Adds new customer to database
+     * 
+     * @param \Application\Entity\Customer $customer Customer instance
+     * @return bool True on success and false on failure
+     */
     public function insert(Customer $customer)
     {
+        $columns = $customer->entityToArray();
+        
+        unset($columns['id']);
 
+        $insert = 'INSERT INTO ' . $customer::TABLE . ' ';
+        $queryColumns = '';
+        $queryValues = '';
+
+        foreach ($columns as $column => $value) {
+            $queryColumns .= $column . ',';
+            $queryValues .= '\'' . $value . '\',';
+        }
+
+        $queryColumns = substr($queryColumns, 0, -1);
+        $queryValues = substr($queryValues, 0, -1);
+        $query = $insert . '(' . $queryColumns . ')' . ' VALUES ' . '(' . $queryValues . ')';
+
+        return $this->execute($query);
     }
 
+    /**
+     * Updates existing customer in the database
+     * 
+     * @param \Application\Entity\Customer $customer Customer instance
+     * @return bool True on success and false on failure
+     */
     public function update(Customer $customer)
     {
+        $columns = $customer->entityToArray();
+        $update = 'UPDATE ' . $customer::TABLE;
+        $where = ' WHERE id = ' . $customer->getId();
 
+        unset($columns['id']);
+
+        $update .= ' SET ';
+
+        foreach ($columns as $column => $value) {
+            $update .= $column . ' = \'' . $value . '\',';
+        }
+
+        $query = substr($update, 0, -1) . $where;
+
+        return $this->execute($query);
     }
 
-    public function flush()
+    /**
+     * Executes the query
+     * 
+     * @param string $query The query which is being executed
+     * @return bool Return true on success and false on failure
+     */
+    protected function execute(string $query): bool
     {
+        $success = false;
 
+        try {
+            $stmt = $this->connection->pdo->prepare($query);
+            $stmt->execute();
+            $success = true;
+        } catch (PDOException $e) {
+            error_log(__METHOD__ . ':' . __LINE__ . ':' . $e->getMessage());
+            $success = false;
+        } catch (Throwable $e) {
+            error_log(__METHOD__ . ':' . __LINE__ . ':' . $e->getMessage());
+            $success = false;
+        }
+
+        return $success;
     }
 
-    public function remove(Customer $customer)
+    /**
+     * Delete customer
+     * 
+     * @param \Application\Entity\Customer $customer Customer instance
+     * @return bool
+     */
+    public function delete(Customer $customer): bool
     {
-        
+        $query = 'DELETE FROM ' . $customer::TABLE . ' WHERE id = :id';
+        $stmt = $this->connection->pdo->prepare($query);
+        $stmt->execute(['id' => $customer->getId()]);
+        return ($this->findById($customer->getId())) ? false : true;
     }
 }
